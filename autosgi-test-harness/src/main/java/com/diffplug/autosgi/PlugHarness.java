4de772019abc1a8dec6992f64278e83d099a8082
Ned Twigg <ned.twigg@diffplug.com>
2022-02-02 19:39:21 -0800

    Fix the tes-harness build.

--- autosgi-test-harness/src/main/java/com/diffplug/autosgi/PlugHarness.java
+++ autosgi-test-harness/src/main/java/com/diffplug/autosgi/PlugHarness.java
@@ -1,22 +1,30 @@
 /*
- * Copyright (C) 2017-2021 DiffPlug, LLC - All Rights Reserved
- * Unauthorized copying of this file via any medium is strictly prohibited.
- * Proprietary and confidential.
- * Please send any inquiries to Ned Twigg <ned.twigg@diffplug.com>
+ * Copyright (C) 2017-2022 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.diffplug.autosgi;
 
-import java.util.function.Function;
-import java.util.stream.Stream;
-
-import org.junit.jupiter.api.extension.AfterEachCallback;
-import org.junit.jupiter.api.extension.BeforeEachCallback;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.osgi.framework.ServiceReference;
 
 import com.diffplug.common.base.Preconditions;
 import com.diffplug.common.collect.LinkedHashMultimap;
 import com.diffplug.common.collect.Multimap;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.osgi.framework.ServiceReference;
 
 /** Creates a harness for local plugin setup. */
 public class PlugHarness implements BeforeEachCallback, AfterEachCallback {
