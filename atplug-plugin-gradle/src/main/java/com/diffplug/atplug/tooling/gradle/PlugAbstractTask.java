4e6838259db1f805163c7139d73f62d764f7cf15
Ned Twigg <ned.twigg@diffplug.com>
2022-02-09 14:13:00 -0800

    Empty directory workaround which doesn't work so far.

--- atplug-plugin-gradle/src/main/java/com/diffplug/atplug/tooling/gradle/PlugAbstractTask.java
+++ atplug-plugin-gradle/src/main/java/com/diffplug/atplug/tooling/gradle/PlugAbstractTask.java
@@ -20,48 +20,38 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import org.gradle.api.DefaultTask;
-import org.gradle.api.tasks.InputDirectory;
+import org.gradle.api.file.DirectoryProperty;
+import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.Internal;
 
 public abstract class PlugAbstractTask extends DefaultTask {
-	@InputDirectory
-	public File classesFolderA;
+	@InputFiles
+	public abstract DirectoryProperty getClassesFolderA();
 
-	public File getClassesFolderA() {
-		return classesFolderA;
-	}
+	@InputFiles
+	public abstract DirectoryProperty getClassesFolderB();
 
-	@InputDirectory
-	public File classesFolderB;
-
-	public File getClassesFolderB() {
-		return classesFolderB;
-	}
-
-	@InputDirectory
-	public File classesFolderC;
-
-	public File getClassesFolderC() {
-		return classesFolderC;
-	}
+	@InputFiles
+	public abstract DirectoryProperty getClassesFolderC();
 
 	void setClassesFolders(Iterable<File> files) {
 		int size = 0;
 		for (File file : files) {
+			file.mkdirs();
 			switch (++size) {
 			case 1:
 				// set all fields
-				classesFolderA = file;
-				classesFolderB = file;
-				classesFolderC = file;
+				getClassesFolderA().set(file);
+				getClassesFolderB().set(file);
+				getClassesFolderC().set(file);
 				break;
 			case 2:
 				// set the second field
-				classesFolderB = file;
+				getClassesFolderB().set(file);
 				break;
 			case 3:
 				// set the last field
-				classesFolderC = file;
+				getClassesFolderC().set(file);
 				break;
 			default:
 				// nothing
@@ -75,12 +65,12 @@ public abstract class PlugAbstractTask extends DefaultTask {
 	@Internal
 	public List<File> getClassesFolders() {
 		List<File> files = new ArrayList<>();
-		files.add(classesFolderA);
-		if (!classesFolderA.equals(classesFolderB)) {
-			files.add(classesFolderB);
+		files.add(getClassesFolderA().get().getAsFile());
+		if (!getClassesFolderA().equals(getClassesFolderB())) {
+			files.add(getClassesFolderB().get().getAsFile());
 		}
-		if (!classesFolderA.equals(classesFolderC)) {
-			files.add(classesFolderC);
+		if (!getClassesFolderA().equals(getClassesFolderC())) {
+			files.add(getClassesFolderC().get().getAsFile());
 		}
 		return files;
 	}
