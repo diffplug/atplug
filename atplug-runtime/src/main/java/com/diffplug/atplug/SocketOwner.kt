68682804d7baae063d6acff14eae7b5e03c396f1
Ned Twigg <ned.twigg@diffplug.com>
2022-12-30 10:52:07 -0800

    Revert "SocketOwner now gives a more helpful exception when the problem is "java.io.EOFException: Unexpected end of ZLIB input stream""
    This reverts commit d3641a32eaa2f1a3fa71c9105a4dbd1f2ff69f72.

--- atplug-runtime/src/main/java/com/diffplug/atplug/SocketOwner.kt
+++ atplug-runtime/src/main/java/com/diffplug/atplug/SocketOwner.kt
@@ -6,7 +6,6 @@
  */
 package com.diffplug.atplug
 
-import java.io.EOFException
 import java.lang.Exception
 import java.lang.IllegalArgumentException
 import java.lang.RuntimeException
@@ -189,7 +188,6 @@ abstract class SocketOwner<T>(val socketClass: Class<T>) {
 					return generatorForSocket(socket)
 				}
 			} catch (e: Throwable) {
-				rethrowIfEOF(e)
 				firstAttempt = e
 			}
 			try {
@@ -197,19 +195,12 @@ abstract class SocketOwner<T>(val socketClass: Class<T>) {
 				val socket = socketOwnerClass.objectInstance!! as SocketOwner<T>
 				return generatorForSocket(socket)
 			} catch (secondAttempt: Throwable) {
-				rethrowIfEOF(secondAttempt)
 				val e =
 						IllegalArgumentException(
 								"To create metadata for `$socketClass` we need either a field `static final SocketOwner socket` or a kotlin `object Socket`.",
 								secondAttempt)
 				firstAttempt?.let(e::addSuppressed)
 				throw e
-			}
-		}
-
-		private fun rethrowIfEOF(exception: Throwable) {
-			if (rootCause(exception) is EOFException) {
-				throw Error("The JVM needs to be restarted (gradew --stop) to fix", exception)
 			}
 		}
 
